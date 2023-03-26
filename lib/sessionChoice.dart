b43f27ceee0492e8af360b3e617fb8ca384139dc
jaewoo <leejaewoo1230@gmail.com>
2023-03-26 16:03:16 -0700

    Added bottom navigation bar

--- lib/sessionChoice.dart
+++ lib/sessionChoice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import './components/searchBar.dart';
 import './components/componentCards.dart';
 
@@ -6,66 +7,101 @@ class SessionChoiceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Container(
-          color: Color(0xFF15002c),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: SizedBox.expand(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/background/10.jpg'),
-                        fit: BoxFit.fill,
-                      ),
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        color: Color(0xFF15002c),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: SizedBox.expand(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/background/10.jpg'),
+                      fit: BoxFit.fill,
                     ),
                   ),
                 ),
               ),
-              Expanded(
-                // flex: 3,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 12.0, horizontal: 27.0),
-                      child: Text(
-                        'Session Themes',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 39.0,
-                          fontWeight: FontWeight.w800,
-                        ),
+            ),
+            Expanded(
+              // flex: 3,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 27.0),
+                    child: Text(
+                      'Session Themes',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 39.0,
+                        fontWeight: FontWeight.w800,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: SearchBar(),
-                    ),
-                  ],
-                ),
-                // Widget that will return container that contains rows
-                // and columns of contents by inputting number of
-                // rows and columns
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: SearchBar(),
+                  ),
+                ],
               ),
-              Expanded(
-                flex: 3,
-                child: ComponentCards(
-                  cardCol: 2,
-                  cardRow: 3,
-                ),
+              // Widget that will return container that contains rows
+              // and columns of contents by inputting number of
+              // rows and columns
+            ),
+            Expanded(
+              flex: 3,
+              child: ComponentCards(
+                cardCol: 2,
+                cardRow: 3,
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Icon(
+                Icons.home,
+                color: Colors.white,
+                size: 35.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+                size: 35.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Icon(
+                FontAwesomeIcons.crown,
+                color: Colors.white,
+                size: 35.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
